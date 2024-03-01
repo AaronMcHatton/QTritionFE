ab8a04a48281ee5ff4c25a3fde41c8d2083ee2bb
AaronMcHatton <Aaron.McHatton@gmail.com>
2024-03-01 16:32:26 -0600

    Finishing initial env and tooling setup

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: path.resolve(__dirname, '..', './src/index.tsx'),
@@ -39,6 +40,30 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '..', './src/index.html'),
     }),
+    // new CopyPlugin({
+    //   patterns: [
+    //     { from: 'src/assets', to: 'assets' },
+    //   ],
+    // }
+    // {
+    //   from: path.resolve(__dirname, '..', './src/assets'),
+    //   to: path.resolve(__dirname, '..', './build/assets'),
+    // }
+    // {
+    //   from: path.resolve(__dirname, '..', './src/index.html'),
+    //   to: path.resolve(__dirname, '..', './build/index.html'),
+    // }
+    // {
+    //   from: path.resolve(__dirname, '..', './src/favicon.ico'),
+    //   to: path.resolve(__dirname, '..', './build/favicon.ico'),
+    // }
+    // {
+    //   from: path.resolve(__dirname, '..', './src/manifest.json'),
+    //   to: path.resolve(__dirname, '..', './build/manifest.json'),
+    // }
+    // {
+    //   from: path.resolve(__dirname, '..', './src/sw.
+    // })
   ],
   stats: 'errors-only',
 }
