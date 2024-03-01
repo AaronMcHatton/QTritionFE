dd98cf8f481c12e56372555dc3c3b4cbc6174e47
AaronMcHatton <Aaron.McHatton@gmail.com>
2024-03-01 15:15:00 -0600

    adding linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
 export const App = () => {
-    return <h1>QTrition - {process.env.NODE_ENV} {process.env.name}</h1>
-}
+  return (
+    <h1>
+      QTrition - {process.env.NODE_ENV} {process.env.name}
+    </h1>
+  );
+};
